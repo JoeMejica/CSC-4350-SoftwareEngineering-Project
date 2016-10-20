801dd701dddbf7ac3d57513634f60e9672a61a43
sluong4 <seyanaluong@gmail.com>
2016-10-20 17:27:22 -0400

    Add files via upload

--- DepartureEvent.java
+++ DepartureEvent.java
@@ -1,4 +1,67 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DepartureEvent {
+	public void reserveEvent(Item item) {
+		item.setReserved(true);
+	}
 
+	public void pendingEvent(Item item) {
+		if (item.getReserved() == true) {
+			item.setPending(true);
+		} else {
+			item.setPending(false);
+		}
+	}
+
+	public void readyEvent(Item item) {
+		if (item.getReserved() == true && item.getPending() == true) {
+			item.setReady(true);
+		} else {
+			item.setReady(false);
+		}
+	}
+
+	public void shipEvent(Item item) {
+		if (item.getReserved() == true && item.getPending() == true && item.getReady() == true) {
+			item.setShipped(true);
+		} else {
+			item.setShipped(false);
+		}
+	}
+
+	// Need database connection
+	public void removeShippedItem(Item item, Connection conn) {
+		if (item.getShipped() == true) {
+			try {
+				Barcode barcode = item.getBarcode();
+				String removeBarcode = barcode.getBarcode();
+				String query = "DELETE FROM items WHERE barcode=?";
+				PreparedStatement preparedStmt = conn.prepareStatement(query);
+				preparedStmt.setString(1, removeBarcode);
+				preparedStmt.executeUpdate();
+			} catch (SQLException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
+
+	// Need database connection
+	public void updateDepartureEvents(Item item, Connection conn) {
+		try {
+			Barcode barcode = item.getBarcode();
+			String addBarcode = barcode.getBarcode();
+			String query = "UPDATE items SET reserved=?, pending=?, ready=?, shipped=? WHERE barcode=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			preparedStmt.setBoolean(1, item.getReserved());
+			preparedStmt.setBoolean(2, item.getPending());
+			preparedStmt.setBoolean(3, item.getReady());
+			preparedStmt.setBoolean(4, item.getShipped());
+			preparedStmt.setString(5, addBarcode);
+			preparedStmt.executeUpdate();
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
 }
--- Item.java
+++ Item.java
@@ -1,26 +1,62 @@
 
 public class Item {
-	 private Barcode itemBarcode;
-	 double itemWeight;
-	 private String expirationDate;//if null there is no expiration date
-	 private boolean reserved;
-	 public Item(double weight,String expirationDate){
-		 reserved= false;
-		 this.itemBarcode=itemBarcode;
-	 }
-	 public Barcode getBarcode(){
-		 return itemBarcode;
-	 }
-	 public String getExpirationDate(){
-		 return expirationDate;
-	 }
-	 public boolean getReserved(){
-		 return reserved;
-	 }
-	 public void setExpirationDate(String Date){
-		 expirationDate=Date;
-	 }
-	 public void setReserved(boolean reserve){
-		 reserved=reserve;
-	 }
+	private Barcode itemBarcode;
+	double itemWeight;
+	private String expirationDate;// if null there is no expiration date
+	private boolean reserved;
+	private boolean pending;
+	private boolean ready;
+	private boolean shipped;
+
+	public Item(double weight, String expirationDate) {
+		reserved = false;
+		pending = false;
+		ready = false;
+		shipped = false;
+		this.itemBarcode = itemBarcode;
+	}
+
+	public Barcode getBarcode() {
+		return itemBarcode;
+	}
+
+	public String getExpirationDate() {
+		return expirationDate;
+	}
+
+	public boolean getReserved() {
+		return reserved;
+	}
+
+	public boolean getPending() {
+		return pending;
+	}
+
+	public boolean getReady() {
+		return ready;
+	}
+
+	public boolean getShipped() {
+		return shipped;
+	}
+
+	public void setExpirationDate(String Date) {
+		expirationDate = Date;
+	}
+
+	public void setReserved(boolean reserve) {
+		reserved = reserve;
+	}
+
+	public void setPending(boolean pending) {
+		this.pending = pending;
+	}
+
+	public void setReady(boolean ready) {
+		this.ready = ready;
+	}
+
+	public void setShipped(boolean shipped) {
+		this.shipped = shipped;
+	}
 }
