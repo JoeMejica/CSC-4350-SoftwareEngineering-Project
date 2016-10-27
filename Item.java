2175a7ffbdc00898e5341f3bb5c2b0284b7fee16
crice8 <crice8@student.gsu.edu>
2016-10-26 23:27:57 -0400

    Add files via upload

--- Item.java
+++ Item.java
@@ -1,62 +1,47 @@
 
 public class Item {
-	private Barcode itemBarcode;
-	double itemWeight;
-	private String expirationDate;// if null there is no expiration date
-	private boolean reserved;
-	private boolean pending;
-	private boolean ready;
-	private boolean shipped;
-
-	public Item(double weight, String expirationDate) {
-		reserved = false;
-		pending = false;
-		ready = false;
-		shipped = false;
-		this.itemBarcode = itemBarcode;
-	}
-
-	public Barcode getBarcode() {
-		return itemBarcode;
-	}
-
-	public String getExpirationDate() {
-		return expirationDate;
-	}
-
-	public boolean getReserved() {
-		return reserved;
-	}
-
-	public boolean getPending() {
-		return pending;
-	}
-
-	public boolean getReady() {
-		return ready;
-	}
-
-	public boolean getShipped() {
-		return shipped;
-	}
-
-	public void setExpirationDate(String Date) {
-		expirationDate = Date;
-	}
-
-	public void setReserved(boolean reserve) {
-		reserved = reserve;
-	}
-
-	public void setPending(boolean pending) {
-		this.pending = pending;
-	}
-
-	public void setReady(boolean ready) {
-		this.ready = ready;
-	}
-
-	public void setShipped(boolean shipped) {
-		this.shipped = shipped;
-	}
+	 private Barcode itemBarcode;
+	 private double itemWeight;
+	 private String itemName;
+	 private String expirationDate;//if null there is no expiration date
+	 private boolean reserved;
+	 public Item(String name,double weight,Barcode itemBarcode,String expirationDate){//items with expiration date
+		 reserved= false;
+		 this.itemBarcode=itemBarcode;
+		 itemWeight=weight;
+		 this.expirationDate=expirationDate;
+		 itemName=name;
+	 }
+	 public Item(double weight,Barcode itemBarcode){
+		 reserved= false;
+		 this.itemBarcode=itemBarcode;
+		 itemWeight=weight;
+	 }
+	 public Barcode getBarcode(){
+		 return itemBarcode;
+	 }
+	 public String getExpirationDate(){
+		 return expirationDate;
+	 }
+	 public boolean getReserved(){
+		 return reserved;
+	 }
+	 public double getItemWeight(){
+		 return itemWeight;
+	 }
+	 public String getItemName(){
+		 return itemName;
+	 }
+	 public void setExpirationDate(String Date){
+		 expirationDate=Date;
+	 }
+	 public void setReserved(boolean reserve){
+		 reserved=reserve;
+	 }
+	 public void setItemWeight(double weight){
+		 itemWeight=weight;
+	 }
+	 public void setItemName(String name){
+		 itemName=name;
+	 }
 }
