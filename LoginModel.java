b1dbee21207b823df73468b45593ead057e16e0f
sluong4 <seyanaluong@gmail.com>
2016-11-01 02:33:26 -0400

    Add files via upload

---
+++ LoginModel.java
@@ -0,0 +1,52 @@
+package InventoryManagementSystem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class LoginModel {
+	Connection conn;
+	public LoginModel() {
+		conn = SQLiteConnection.Connector();
+		if (conn == null) {
+			System.out.println("Connection not successful");
+			System.exit(1);
+		}
+
+	}
+
+	public boolean isDBconnected() {
+		try {
+			return !conn.isClosed();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean isLogin(String username, String password) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String query = "SELECT * FROM employee WHERE username = ? AND password = ?";
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, username);
+			pstmt.setString(2, password);
+			rset = pstmt.executeQuery();
+			if (rset.next()) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			return false;
+		} finally {
+			pstmt.close();
+			rset.close();
+		}
+	}
+	
+}
