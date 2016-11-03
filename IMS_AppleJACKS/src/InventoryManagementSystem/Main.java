d86509247d1b450bd9bd76ec6333a06647e8af9a
sluong4 <seyanaluong@gmail.com>
2016-11-03 19:16:43 -0400

    Add files via upload

--- IMS_AppleJACKS/src/InventoryManagementSystem/Main.java
+++ IMS_AppleJACKS/src/InventoryManagementSystem/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("manageInventory.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("I.M.S. | Login");
         primaryStage.setScene(new Scene(root, 800, 500));
 
